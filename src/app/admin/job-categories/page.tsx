29bfe7e65ea8de893bc94a985dcc58c003cc3b98
Jospin <jospinndagano1@gmail.com>
2025-04-01 16:21:30 +0200

    create admin pages

---
+++ src/app/admin/job-categories/page.tsx
@@ -0,0 +1,8 @@
+import JobCategoryForm from "@/components/forms/jobCategoryForm";
+
+export default function JobCategories() {
+    return <div>
+        <h1>Add Category</h1>
+        <JobCategoryForm />
+    </div>
+}
