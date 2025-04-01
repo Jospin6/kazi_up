80154d8330ab76a7045d885be4cecaf540e7f807
Jospin <jospinndagano1@gmail.com>
2025-04-01 13:47:14 +0200

    add types

---
+++ src/helpers/types.ts
@@ -0,0 +1,113 @@
+export interface User {
+    id: string;
+    username: string;
+    email: string;
+    password?: string;
+    accountType?: AccountType;
+    role?: UserRole;
+    avatar?: Uint8Array;
+    location?: string;
+    residencyCountry?: string;
+    nationality?: string;
+    gender?: string;
+    website?: string;
+    github?: string;
+    twitter?: string;
+    linkedin?: string;
+    bio?: string;
+    skills?: string;
+    languages?: string;
+    available?: string;
+    timezone?: string;
+    annualpay?: string;
+    hourlypay?: string;
+    companyId?: Company;
+    jobs?: Job[];
+    createdAt?: Date;
+    updatedAt?: Date;
+    UserActivity?: UserActivity[];
+}
+
+export interface Company {
+    id: string;
+    name: string;
+    description?: string;
+    logo?: string;
+    website?: string;
+    location?: string;
+    industry?: string;
+    foundedYear?: number;
+    employees?: number;
+    owner: User;
+    ownerId: string;
+    jobs?: Job[];
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface JobCategory {
+    id: string;
+    title: string;
+    tags: string;
+    jobs: Job[];
+}
+
+export interface EmploymentType {
+    id: string;
+    title: string;
+    jobs: Job[];
+}
+
+export interface Job {
+    id: string;
+    title: string;
+    description: string;
+    salary?: string;
+    location?: string;
+    remote: boolean;
+    employementTypeId: string;
+    employementType: EmploymentType;
+    tags: string;
+    keywords: string;
+    company: Company;
+    companyId: string;
+    postedBy: User;
+    postedById: string;
+    jobCategoryId: string;
+    jobCategory: JobCategory;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface UserActivity {
+    id: string;
+    type: ActivityType;
+    yearStart?: string;
+    yearEnd?: string;
+    title?: string;
+    company?: string;
+    url?: string;
+    email?: string;
+    description?: string;
+    user: User;
+    userId: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export enum UserRole {
+    USER = "USER",
+    ADMIN = "ADMIN",
+    SUPERADMIN = "SUPERADMIN"
+}
+
+export enum AccountType {
+    EMPLOYEE = "EMPLOYEE",
+    COMPANY = "COMPANY"
+}
+
+export enum ActivityType {
+    EXPERIENCE = "EXPERIENCE",
+    EDUCATION = "EDUCATION",
+    SIDE_PROJECT = "SIDE_PROJECT"
+}
