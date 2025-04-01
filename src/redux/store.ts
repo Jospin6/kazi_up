d44200ead38e8a8952a17c87acf583a0abc13537
Jospin <jospinndagano1@gmail.com>
2025-04-01 17:08:53 +0200

    add slices to the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './auth/authSlice'
+import jobReducer from './job/jobSlice'
+import companyReducer from './company/companySlice'
+import jobCategoryReducer from './jobCategory/jobCategorySlice'
+import userReducer from './user/userSlice'
+import userActivityReducer from './userActivity/userActivitySlice'
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        job: jobReducer,
+        company: companyReducer,
+        jobCategory: jobCategoryReducer,
+        user: userReducer,
+        userActivity: userActivityReducer
     }
 })
 
