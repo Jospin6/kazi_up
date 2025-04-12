a2e9e8a885fd2477589f176784169887ad04591c
Jospin <jospinndagano1@gmail.com>
2025-04-12 13:32:33 +0200

    create footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,10 @@
+
+export const Footer = () => {
+    return <div className="w-full h-[80px] bg-[#1f1f1f]">
+        <div> 2025 KaziUp </div>
+        <div>
+            <p>why choose us</p>
+            <p>our social media</p>
+        </div>
+    </div>
+}
